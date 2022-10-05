9373437b64bbbcfefbd7c44259130527a20022aa
Giovanna Italiani <giovannaitaliani@gmail.com>
2022-10-05 16:31:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Welcome from './components/Welcome';
+import Question from './components/Question';
+import { useContext, useEffect } from 'react';
+import { QuizContext } from './context/quiz';
+import GameOver from './components/GameOver';
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch({ type: 'REORDER_QUESTIONS' });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Quiz de Progamação</h1>
+      {quizState.gameStage === 'Start' && <Welcome />}
+      {quizState.gameStage === 'Playing' && <Question />}
+      {quizState.gameStage === 'End' && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
